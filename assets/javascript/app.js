f0827fb730d4b2dc98fb2e96adf6b700763f967d
MCardenas38 <41306780+MCardenas38@users.noreply.github.com>
2019-03-08 21:15:05 -0500

    Enter and Search button both work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-  // Golobal Varibale
+  // Global Varibale
   var eventsArray= [];
-  var markers;  
-  
+  var markers; 
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  var image = 'http://maps.google.com/mapfiles/kml/shapes/man.png';
+
   // set up current time
 
     function timer() {
@@ -21,18 +24,6 @@ $(document).ready(function(){
         });
     }
     timer();
-
-    // function searchWeather(weather) {
-    //   var queryURLTwo = "api.openweathermap.org/data/2.5/weather?q=" + inputCity;
-    //   $.ajax({
-    //     url: queryURLTwo,
-    //     method: "GET"
-    //   }).then(function(result){
-    //     var cityWeather = {
-
-    //     }
-    //   })
-    // }
 
 
     function searchEventsNearMe(address, price, category, radius) {
@@ -109,20 +100,21 @@ $(document).ready(function(){
             });
       }
     })
+
+
+
   
     // Event handler for user clicking the get-address button
+
+
     $("#search-btn").on("click", function(event) {
       // Preventing the button from trying to submit the form
       event.preventDefault();
       // Storing the address
-      var inputAddress = $("#inputAddress").val().trim();
-      var inputCity = $("#inputCity").val().trim();
-      var inputState = $("#inputState").val().trim();
-      var inputZip = $("#inputZip").val().trim();
-      var combinedAddress = inputAddress+ "" + inputCity + "" + inputState + "" + inputZip;
+      var inputAddress = $("#pac-input").val().trim();
 
       //sort by price
-
+      var inputPrice;
       if ($("#priceRange").val() === "1") {
         inputPrice = "free";
       } else if ($("#priceRange").val() === "2") {
@@ -132,7 +124,7 @@ $(document).ready(function(){
       }
 
       // sort by categories
-
+      var inputCategory;
       if ($("#inputCategories").val() === "1") {
         inputCategory = "104";
       } else if ($("#inputCategories").val() === "2") {
@@ -149,7 +141,7 @@ $(document).ready(function(){
         inputCategory = "104,113,108,103,110,119";
       };
 
-
+      var inputRadius;
       //Radius input
         if ($("#inputRadius").val() === "1") {
           var inputRadius = "1mi";
@@ -160,10 +152,158 @@ $(document).ready(function(){
       } else {
           var inputRadius = "1mi";
       }
-  
-      console.log(combinedAddress);
+
       // Running the searchEventsNearMe function(passing in the address as an argument)
-      searchEventsNearMe(combinedAddress, inputPrice, inputCategory, inputRadius);
+      searchEventsNearMe(inputAddress, inputPrice, inputCategory, inputRadius);
+      google.maps.event.trigger(input, 'focus', {});
+      google.maps.event.trigger(input, 'keydown', { keyCode: 13 });
+      google.maps.event.trigger(this, 'focus', {});
     });
 
+
+
+
+
+
+    // NEW SEARCH BOX MAP FUNCTION BY MATTHEW A
+
+
+    // Relate traffic layer variable to an option button
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    // Relate transit layer variable to an option button 
+    var transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var point = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    function add_homemarker(){
+      var places = searchBox.getPlaces();
+      console.log(places);
+      
+
+      // Storing the address
+      var inputAddress = $("#pac-input").val().trim();
+
+      //sort by price
+      var inputPrice;
+      if ($("#priceRange").val() === "1") {
+        inputPrice = "free";
+      } else if ($("#priceRange").val() === "2") {
+        inputPrice = "paid";
+      } else {
+        inputPrice = false;
+      }
+
+      // sort by categories
+      var inputCategory;
+      if ($("#inputCategories").val() === "1") {
+        inputCategory = "104";
+      } else if ($("#inputCategories").val() === "2") {
+        inputCategory = "113";
+      } else if ($("#inputCategories").val() === "3") {
+        inputCategory = "108";
+      } else if ($("#inputCategories").val() === "4") {
+        inputCategory = "103";
+      } else if ($("#inputCategories").val() === "5") {
+        inputCategory = "110";
+      } else if ($("#inputCategories").val() === "6") {
+        inputCategory = "119"; 
+      } else {
+        inputCategory = "104,113,108,103,110,119";
+      };
+
+      var inputRadius;
+      //Radius input
+        if ($("#inputRadius").val() === "1") {
+          var inputRadius = "1mi";
+      } else if ($("#inputRadius").val() === "2") {
+          var inputRadius = "2mi";
+      } else if ($("#inputRadius").val() === "3") {
+          var inputRadius = "3mi";
+      } else {
+          var inputRadius = "1mi";
+      }
+
+      // Running the searchEventsNearMe function(passing in the address as an argument)
+      searchEventsNearMe(inputAddress, inputPrice, inputCategory, inputRadius);
+
+
+
+      // ADDED FOR SEARCH BOX RESULTS
+      var startLoc = $("#pac-input").val();
+      $("#pac-input").val('');
+      address= startLoc;
+      console.log(startLoc);  
+      // END
+      var inputAddress = $("#pac-input").val().trim();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      point.forEach(function(marker) {
+      
+        marker.setMap(null);
+        marker= null;
+      });
+      point = [];
+
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+
+        };
+
+        // Create a custome marker for user start location .
+        point.push(new google.maps.Marker({
+          animation: google.maps.Animation.DROP,
+          map: map,
+          icon: image,
+          label:"YOU",
+          label: {
+                    text: "YOU",
+                    color: "#eb3a44",
+                    fontSize: "30px",
+                    fontWeight: "bold"
+                  
+                },
+                labelInBackground: true,
+          title: "place.name",
+          position: place.geometry.location
+
+        }));
+
+        if (place.geometry.viewport) { 
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+      
+        }
+      });
+      map.fitBounds(bounds);
+
+    }
+
+    searchBox.addListener('places_changed', add_homemarker);
 });
